eeea5352a67ca41b053953cbc3cda8bb45e5907a
Pavel <112804487+psok712@users.noreply.github.com>
2024-04-17 12:40:29 +0300

    [CSJHW-7]: init

---
+++ src/Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure/Migrations/20240417122100_Initial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentMigrator;
+
+using Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure.Common;
+
+namespace Ozon.Route256.Postgres.Persistence.Migrations;
+
+[Migration(20240417122100, TransactionBehavior.None)]
+public sealed class Initial : SqlMigration
+{
+    protected override string GetUpSql(IServiceProvider services) => @"
+create table if not exists goods_accounting 
+(   id                     bigserial   not null 
+  , status                 text        not null
+  , amount                 bigint      not null
+  , update_at              timestamptz null
+  , CONSTRAINT statuses_pk primary key (id));
+";
+
+    protected override string GetDownSql(IServiceProvider services) => @"
+drop table if exists goods_accounting;
+";
+}
