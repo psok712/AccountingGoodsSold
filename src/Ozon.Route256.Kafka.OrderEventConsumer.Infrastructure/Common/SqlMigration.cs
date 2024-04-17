fc7c532775ca24e384bfdea056819b5eb526632b
Pavel <112804487+psok712@users.noreply.github.com>
2024-04-17 12:40:55 +0300

    [CSJHW-7]: adding down migration

--- src/Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure/Common/SqlMigration.cs
+++ src/Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure/Common/SqlMigration.cs
@@ -15,9 +15,16 @@ public abstract class SqlMigration : IMigration
         context.Expressions.Add(new ExecuteSqlStatementExpression { SqlStatement = GetUpSql(context.ServiceProvider) });
     }
 
-    public void GetDownExpressions(IMigrationContext context) => throw new NotSupportedException();
+    public void GetDownExpressions(IMigrationContext context)
+    {
+        _ = context ?? throw new ArgumentNullException(nameof(context));
+
+        context.Expressions.Add(new ExecuteSqlStatementExpression { SqlStatement = GetDownSql(context.ServiceProvider) });
+    }
 
     protected abstract string GetUpSql(IServiceProvider services);
+    
+    protected abstract string GetDownSql(IServiceProvider services);
 
     object IMigration.ApplicationContext => throw new NotSupportedException();
     string IMigration.ConnectionString => throw new NotSupportedException();
