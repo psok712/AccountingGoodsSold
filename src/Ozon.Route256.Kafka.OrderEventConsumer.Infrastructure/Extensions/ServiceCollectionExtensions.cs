188b97c295df750f9c6f25c512532a1465ee9599
Pavel <112804487+psok712@users.noreply.github.com>
2024-04-17 23:11:41 +0300

    [CSJHW-7]: add npgsql data source infrastructure repositories

--- src/Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,18 +1,42 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ozon.Route256.Kafka.OrderEventConsumer.Domain;
+using Ozon.Route256.Kafka.OrderEventConsumer.Domain.Entities;
+using Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure.Repositories;
 using Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure.Settings;
 
 namespace Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddBllInfrastructure(
+    public static IServiceCollection AddInfrastructureRepositories(
+        this IServiceCollection services)
+    {
+        AddPostgresRepositories(services);
+        
+        return services;
+    }
+    
+    private static void AddPostgresRepositories(IServiceCollection services)
+    {
+        services.AddSingleton<IItemRepository, ItemRepository>();
+    }
+    
+    public static IServiceCollection AddInfrastructure(
         this IServiceCollection services, 
         IConfiguration config)
     {
-        //read config
+        Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+        
         services.Configure<KafkaOptions>(config.GetSection(nameof(KafkaOptions)));
         services.Configure<KafkaConsumerOptions>(config.GetSection(nameof(KafkaConsumerOptions)));
+
+        services.AddNpgsqlDataSource(
+            config.GetSection("ConnectionPostgresString").Value,
+            builder =>
+            {
+                builder.MapComposite<ItemEntityV1>("item_v1", builder.DefaultNameTranslator);
+            });
         
         return services;
     }
