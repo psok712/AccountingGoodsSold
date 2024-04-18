90584b80a409be505fafada2e33f3a298f6dc9a5
Pavel <112804487+psok712@users.noreply.github.com>
2024-04-18 21:34:16 +0300

    [CSJHW-7]: change method for incrementing

--- src/Ozon.Route256.Kafka.OrderEventConsumer.Domain/Interfaces/IItemRepository.cs
+++ src/Ozon.Route256.Kafka.OrderEventConsumer.Domain/Interfaces/IItemRepository.cs
@@ -6,9 +6,13 @@ namespace Ozon.Route256.Kafka.OrderEventConsumer.Domain.Interfaces;
 
 public interface IItemRepository
 {
-    Task Add(ItemEntityV1 item, CancellationToken token);
+    Task AddIfNotExist(long itemId, CancellationToken token);
 
+    Task IncCreated(long itemId, CancellationToken token);
+    
+    Task IncCanceled(long itemId, CancellationToken token);
+    
+    Task IncDelivered(long itemId, CancellationToken token);
+    
     Task<ItemEntityV1> Get(long itemId, CancellationToken token);
-
-    Task Update(ItemEntityV1 updateModel, CancellationToken token);
 }
