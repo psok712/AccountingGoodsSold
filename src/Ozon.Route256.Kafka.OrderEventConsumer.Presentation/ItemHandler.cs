f83531d425ea14f2a3a24e342b1bda707f5c3d2d
Pavel <112804487+psok712@users.noreply.github.com>
2024-04-19 14:12:26 +0300

    [CSJHW-7]: added implements handler

--- src/Ozon.Route256.Kafka.OrderEventConsumer.Presentation/ItemHandler.cs
+++ src/Ozon.Route256.Kafka.OrderEventConsumer.Presentation/ItemHandler.cs
@@ -1,26 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 using Microsoft.Extensions.Logging;
+using Ozon.Route256.Kafka.OrderEventConsumer.Domain.Interfaces;
+using Ozon.Route256.Kafka.OrderEventConsumer.Domain.OrderProduce;
 using Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure.Kafka;
 
 namespace Ozon.Route256.Kafka.OrderEventConsumer.Presentation;
 
-public class ItemHandler : IHandler<Ignore, string>
+public class ItemHandler : IHandler<long, OrderEventProduce>
 {
     private readonly ILogger<ItemHandler> _logger;
-    private readonly Random _random = new();
+    private readonly IItemService _service;
 
-    public ItemHandler(ILogger<ItemHandler> logger)
+    public ItemHandler(ILogger<ItemHandler> logger, IItemService service)
     {
         _logger = logger;
+        _service = service;
     }
 
-    public async Task Handle(IReadOnlyCollection<ConsumeResult<Ignore, string>> messages, CancellationToken token)
+    public async Task Handle(IReadOnlyCollection<ConsumeResult<long, OrderEventProduce>> messages,
+        CancellationToken token)
     {
-        await Task.Delay(_random.Next(300), token);
+        var tasks = messages
+            .Select(async m => await _service.OrderProcessing(m.Message.Value, token))
+            .ToList();
+        await Task.WhenAll(tasks);
+
         _logger.LogInformation("Handled {Count} messages", messages.Count);
     }
 }
