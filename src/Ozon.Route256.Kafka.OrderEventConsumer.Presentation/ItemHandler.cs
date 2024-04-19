e93576252c116d0ceb401d9128bab47acc4abeab
Pavel <112804487+psok712@users.noreply.github.com>
2024-04-19 20:14:10 +0300

    [CSJHW-7]: add method to convert OrderEventProduce to OrderEvent

--- src/Ozon.Route256.Kafka.OrderEventConsumer.Presentation/ItemHandler.cs
+++ src/Ozon.Route256.Kafka.OrderEventConsumer.Presentation/ItemHandler.cs
@@ -4,18 +4,21 @@ using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 using Microsoft.Extensions.Logging;
+using Ozon.Route256.Kafka.OrderEventConsumer.Domain.Enums;
 using Ozon.Route256.Kafka.OrderEventConsumer.Domain.Interfaces;
-using Ozon.Route256.Kafka.OrderEventConsumer.Domain.OrderProduce;
+using Ozon.Route256.Kafka.OrderEventConsumer.Domain.Order;
+using Ozon.Route256.Kafka.OrderEventConsumer.Domain.ValueObjects;
 using Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure.Kafka;
+using Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure.OrderProduce;
 
 namespace Ozon.Route256.Kafka.OrderEventConsumer.Presentation;
 
 public class ItemHandler : IHandler<long, OrderEventProduce>
 {
     private readonly ILogger<ItemHandler> _logger;
-    private readonly IItemService _service;
+    private readonly IKafkaService _service;
 
-    public ItemHandler(ILogger<ItemHandler> logger, IItemService service)
+    public ItemHandler(ILogger<ItemHandler> logger, IKafkaService service)
     {
         _logger = logger;
         _service = service;
@@ -25,10 +28,63 @@ public class ItemHandler : IHandler<long, OrderEventProduce>
         CancellationToken token)
     {
         var tasks = messages
-            .Select(async m => await _service.OrderProcessing(m.Message.Value, token))
+            .Select(async m =>
+                await _service.OrderProcessing(OrderEventProduceToOrderEvent(m.Message.Value), token))
             .ToList();
         await Task.WhenAll(tasks);
 
         _logger.LogInformation("Handled {Count} messages", messages.Count);
+    }
+
+    private OrderEvent OrderEventProduceToOrderEvent(OrderEventProduce orderEventProduce)
+    {
+        var positions = orderEventProduce.Positions
+            .Select(p =>
+                new OrderEventPosition(CreateItemId(p.ItemId), p.Quantity, CreateMoney(p.Price)))
+            .ToArray();
+
+        return new OrderEvent(
+            CreateOrderId(orderEventProduce.OrderId),
+            CreateUserId(orderEventProduce.UserId),
+            CreateWarehouse(orderEventProduce.WarehouseId),
+            CreateStatus(orderEventProduce.Status),
+            orderEventProduce.Moment,
+            positions
+        );
+    }
+
+    private OrderId CreateOrderId(long orderId)
+    {
+        return new OrderId(orderId);
+    }
+
+    private UserId CreateUserId(long userId)
+    {
+        return new UserId(userId);
+    }
+
+    private ItemId CreateItemId(long itemId)
+    {
+        return new ItemId(itemId);
+    }
+
+    private WarehouseId CreateWarehouse(long warehouseId)
+    {
+        return new WarehouseId(warehouseId);
+    }
+
+    private Money CreateMoney(MoneyProduce money)
+    {
+        const long mathFraction = 1_000_000_000;
+        return new Money
+        {
+            Value = money.Units + (decimal)money.Nanos / mathFraction,
+            Currency = money.Currency
+        };
+    }
+
+    private Status CreateStatus(OrderStatus orderStatus)
+    {
+        return (Status)orderStatus;
     }
 }
