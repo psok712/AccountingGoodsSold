9b1cc6e7d2a4d0e08f817dc09bf91cd3e363fe3a
Pavel <112804487+psok712@users.noreply.github.com>
2024-04-19 20:09:42 +0300

    [CSJHW-7]: testing all method repository

---
+++ Ozon.Route256.Kafka.OrderEventConsumer.IntegrationTests/RepositoryTests/SalesRepositoryTests.cs
@@ -0,0 +1,87 @@
+using FluentAssertions;
+using Ozon.Route256.Kafka.OrderEventConsumer.Domain.Interfaces;
+using Ozon.Route256.Kafka.OrderEventConsumer.IntegrationTests.Fakers;
+using Ozon.Route256.Kafka.OrderEventConsumer.IntegrationTests.Fixtures;
+
+namespace Ozon.Route256.Kafka.OrderEventConsumer.IntegrationTests.RepositoryTests;
+
+[Collection(nameof(TestFixture))]
+public class SalesRepositoryTests
+{
+    private readonly ISalesRepository _repository;
+
+    public SalesRepositoryTests(TestFixture fixture)
+    {
+        _repository = fixture.SalesRepository;
+    }
+
+    [Fact]
+    public async Task AddIfNotExist_Success()
+    {
+        // Arrange
+        var sale = SalesAddModelFaker.Generate().First();
+        var expectedSale = SalesEntityV1Faker.Generate().First()
+            .WithItemId(sale.ItemId)
+            .WithSellerId(sale.SellerId)
+            .WithCurrency(sale.Currency);
+
+
+        // Act
+        await _repository.AddIfNotExist(sale, default);
+
+
+        // Asserts
+        var getModel = SalesGetModelFaker.Generate().First()
+            .WithItemId(sale.ItemId)
+            .WithSellerId(sale.SellerId);
+        var resultSale = await _repository.Get(getModel, default);
+        resultSale.Should().BeEquivalentTo(expectedSale);
+    }
+
+    [Fact]
+    public async Task Get_AddSale_ShouldReturnThisSale()
+    {
+        // Arrange
+        var sale = SalesAddModelFaker.Generate().First();
+        await _repository.AddIfNotExist(sale, default);
+        var expectedSale = SalesEntityV1Faker.Generate().First()
+            .WithItemId(sale.ItemId)
+            .WithSellerId(sale.SellerId)
+            .WithCurrency(sale.Currency);
+
+
+        // Act
+        var getModel = SalesGetModelFaker.Generate().First()
+            .WithItemId(sale.ItemId)
+            .WithSellerId(sale.SellerId);
+        var resultSale = await _repository.Get(getModel, default);
+
+
+        // Asserts
+        resultSale.Should().BeEquivalentTo(expectedSale);
+    }
+
+    [Fact]
+    public async Task IncSale_Success()
+    {
+        // Arrange
+        var saleAdd = SalesAddModelFaker.Generate().First();
+        await _repository.AddIfNotExist(saleAdd, default);
+
+
+        // Act
+        var incModel = SalesIncModelFaker.Generate().First()
+            .WithItemId(saleAdd.ItemId)
+            .WithSellerId(saleAdd.SellerId);
+        var expectedSales = incModel.Price;
+        await _repository.IncSale(incModel, default);
+
+
+        // Asserts
+        var getModel = SalesGetModelFaker.Generate().First()
+            .WithItemId(saleAdd.ItemId)
+            .WithSellerId(saleAdd.SellerId);
+        var sale = await _repository.Get(getModel, default);
+        sale.Sales.Should().Be(expectedSales);
+    }
+}
