6cb78c0b2c17df38818c1a0c7d7bd222cb5f596c
Pavel <112804487+psok712@users.noreply.github.com>
2024-04-20 23:50:13 +0300

    [CSJHW-7]: edit method repo and delete transaction

--- src/Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure/Services/KafkaService.cs
+++ src/Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure/Services/KafkaService.cs
@@ -2,7 +2,6 @@ using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Transactions;
 using Ozon.Route256.Kafka.OrderEventConsumer.Domain.Entities;
 using Ozon.Route256.Kafka.OrderEventConsumer.Domain.Interfaces;
 using Ozon.Route256.Kafka.OrderEventConsumer.Domain.Models;
@@ -20,49 +19,34 @@ public class KafkaService(
     {
         await Task.WhenAll(orderEvent.Positions.Select(async position =>
         {
-            var itemId = position.ItemId.Value;
-            
-            using var transaction = CreateTransactionScope();
-            await itemRepository.AddIfNotExist(itemId, token);
+            var updateModel = new ItemUpdateModel
+            {
+                ItemId = position.ItemId.Value,
+                Quantity = position.Quantity
+            };
 
             switch (orderEvent.Status)
             {
                 case Status.Created:
-                    await itemRepository.IncCreated(itemId, token);
+                    await itemRepository.AddUpdateCreated(updateModel, token);
                     break;
 
                 case Status.Delivered:
                 {
-                    await itemRepository.IncDelivered(itemId, token);
+                    await itemRepository.AddUpdateDelivered(updateModel, token);
                     
-                    await salesRepository.AddIfNotExist(CreateSalesAddModel(position), token);
-                    await salesRepository.IncSale(CreateSalesEntity(position), token);
+                    await salesRepository.AddUpdateSale(CreateSalesEntity(position), updateModel.Quantity, token);
                     break;
                 }
 
                 case Status.Canceled:
-                    await itemRepository.IncCanceled(itemId, token);
+                    await itemRepository.AddUpdateCanceled(updateModel, token);
                     break;
 
                 default:
                     throw new ArgumentException("status doesn't exist");
             }
-            
-            transaction.Complete();
         }));
-    }
-
-    private SalesAddModel CreateSalesAddModel(OrderEventPosition orderEvent)
-    {
-        var sellerId = long.Parse(orderEvent.ItemId.Value.ToString()[..6]);
-        var itemId = long.Parse(orderEvent.ItemId.Value.ToString()[6..]);
-
-        return new SalesAddModel
-        {
-            Currency = orderEvent.Price.Currency,
-            ItemId = itemId,
-            SellerId = sellerId
-        };
     }
 
     private SalesEntityV1 CreateSalesEntity(OrderEventPosition orderEvent)
@@ -77,20 +61,5 @@ public class KafkaService(
             Currency = orderEvent.Price.Currency,
             Price = orderEvent.Price.Value
         };
-    }
-
-    private static TransactionScope CreateTransactionScope(
-        IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
-    {
-        var options = new TransactionOptions
-        {
-            IsolationLevel = isolationLevel,
-            Timeout = TimeSpan.FromSeconds(5)
-        };
-        
-        return new TransactionScope(
-            TransactionScopeOption.Required, 
-            options, 
-            TransactionScopeAsyncFlowOption.Enabled);
     }
 }
